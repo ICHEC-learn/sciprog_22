fe1b21d251e9827db9015ec17e7732816faae4b5
Jia Zhu <jia.zhu@ucdconnect.ie>
2022-10-12 21:02:54 +0100

    practicals from week 2-4 added

--- practical02/Sum.c
+++ practical02/Sum.c
@@ -10,21 +10,23 @@ int main(void) {
 /* First sum */
    sum1 = 0.0;
    for (i=1; i<=1000; i++) {
-      /*  Insert here */
+      sum1 = sum1 + 1.0 / (float)i;
    }
 
 
 /* Second sum */
    sum2 = 0.0;
+   float tmp;
    for (i=1000; i>0; i--) {
-      /* Insert the same line as above except use sum2 */
+      tmp = i;
+      sum2 = sum2 + 1.0/tmp;
    }
 
    printf(" Sum1=%f\n",sum1);
    printf(" Sum2=%f\n",sum2);
 
 /* Find the difference */
-   diff = /* ?? */
+   diff = sum1 - sum2;
 
    printf(" Difference between the two is %f\n",diff);
 
